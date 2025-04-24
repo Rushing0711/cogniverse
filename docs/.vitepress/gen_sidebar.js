5f14a7583d4c7e14322758c2f2c12cf9c50ca558
emon <liming20110711@163.com>
2025-04-24 08:58:25 +0800

    update:20250424-03

--- docs/.vitepress/gen_sidebar.js
+++ docs/.vitepress/gen_sidebar.js
@@ -15,12 +15,14 @@ const intersections = (arr1, arr2) => Array.from(new Set(arr1.filter((item) => !
 /**
  * 递归获取文件夹及其子文件夹中的文件列表，并转换为特定格式的数据
  *
- * @param items 需要处理的文件或文件夹数组
+ * @param files 需要处理的文件或文件夹数组
  * @param dirPath 初始路径
  * @param pathname 当前路径名
  * @returns 格式化后的文件列表数据
  */
-function getList(items, dirPath, pathname) {
+function getList(files, dirPath, pathname) {
+    // 过滤掉
+    const items = intersections(files, WHITE_LIST)
     // 存放结果
     const res = []
     // 开始遍历params
@@ -59,7 +61,5 @@ export const set_sidebar = (pathname) => {
     const dirPath = path.join(DIR_PATH, pathname)
     // 读取 pathname 下的所有文件或者文件夹
     const files = fs.readdirSync(dirPath)
-    // 过滤掉
-    const items = intersections(files, WHITE_LIST)
-    return getList(items, dirPath, pathname);
+    return getList(files, dirPath, pathname);
 }
